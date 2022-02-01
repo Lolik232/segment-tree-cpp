dd191f61be2b79d9d7873cf067f5a8f7456406c5
Vladislav <45938449+Lolik232@users.noreply.github.com>
2022-02-01 23:54:51 +0300

    comment main.cpp

--- main.cpp
+++ main.cpp
@@ -2,42 +2,60 @@
 #include <vector>
 
 #include "segtree/segtree.h"
+#include "segtree/segtree-seg-operation.h"
 
-long long sum(long long a, long long b) {
-    return a + b;
-}
-
-long long min2(long long a, long long b) {
-    return a < b ? a : b;
-}
+//struct MinCnt {
+//	std::size_t count;
+//	int value;
+//};
+//
+//MinCnt combineNodes(const MinCnt& left, const MinCnt& right) {
+//	if (left.value < right.value)
+//		return left;
+//	else if (right.value < left.value)
+//		return right;
+//
+//	return MinCnt {
+//		left.count + right.count,
+//		left.value
+//	};
+//}
 
 int main() {
-    int n, m;
-    std::cin >> n >> m;
-
-    std::vector<long long> segment(n);
-    for (long long& a: segment) {
-        std::cin >> a;
-    }
-
-    Segtree<long long> segmentTree(segment, min2);
-
-    for (int i = 0; i < m; i++) {
-        int currentOperation;
-        std::cin >> currentOperation;
-
-        if (currentOperation == 1) {
-            long long value, pos;
-            std::cin >> pos >> value;
-            segmentTree.set(value, pos);
-        } else {
-            int left, right;
-            std::cin >> left >> right;
-            right -= 1;
-
-            std::cout << segmentTree.query(left, right) << '\n';
-        }
-    }
+//    std::vector<int> segment = {1, 2, 3};
+//    SegtreeSegOperation<int> segtree(segment, segtree_op::SUM, segtree_op::
+//    SUM_NEUTRAL_VAL);
+//
+//    int a = segtree.get(0);
+//	int n, m;
+//	std::cin >> n >> m;
+//
+//
+//	std::vector<MinCnt> segment(n);
+//	for (MinCnt& a : segment) {
+//		std::cin >> a.value;
+//		a.count = 1;
+//	}
+//
+//	Segtree<MinCnt> segmentTree(segment, combineNodes);
+//
+//	for (int i = 0; i < m; i++) {
+//		int currentOperation;
+//		std::cin >> currentOperation;
+//
+//		if (currentOperation == 1) {
+//			int value, pos;
+//			std::cin >> pos >> value;
+//			segmentTree.set(MinCnt { 1, value }, pos);
+//		} else {
+//			int left, right;
+//			std::cin >> left >> right;
+//			right -= 1;
+//
+//			MinCnt res = segmentTree.query(left, right);
+//			std::cout << res.value << ' ' << res.count << '\n';
+//		}
+//	}
 
     return 0;
 }
